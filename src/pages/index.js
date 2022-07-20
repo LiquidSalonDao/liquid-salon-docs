d8be343b8ffd0060e434cdfba5e4f277c428e827
tyral <tyral@zklink.org>
2022-07-20 19:53:36 +0800

    refactor: clean index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,29 +9,12 @@ import { withRouter } from "react-router-dom"
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/Introduction">
-            Liquid Salon DAO Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+  return null
 }
 
 export default withRouter(function Home(props) {
   const {siteConfig} = useDocusaurusContext();
 
-  useEffect(() => {
-    props.history.replace('/docs/Introduction')
-  }, [])
   return null
  
 })
