ec89c3c59eed1172c176fcb0a85611a6764bc175
tyral <tyral@zklink.org>
2022-07-20 18:56:40 +0800

    chore: docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -114,6 +114,7 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: "/welcome/",
           // Please change this to your repo.
           // editUrl:
           //   'https://github.com/facebook/docusaurus/edit/master/website/',
